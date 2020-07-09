20588e0445a0e7747c12b086e95f181a0333d30f
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-09 11:54:36 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars=['snickers','hundred grand','kitkat','skittles'];
+var addElementToBeginningOfArray= (array,element) => {
+  array.unshift(element);
+  return array;
+}
+var destructivelyAddElementToBeginningOfArray=(array,element) =>{
+  array=[element,...array];
+  return array;
+}
+var addElementToEndOfArray= (array,element) =>{
+  array.push(element);
+  return array;
+}
+var destructivelyAddElementToEndOfArray= (array,element) =>{
+  array=[...array,element];
+  return array;
+}
+var accessElementInArray= (array,index) =>{
+  return array[index];
+}
